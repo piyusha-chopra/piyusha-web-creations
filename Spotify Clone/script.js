024dff9a7f5c1032b7be151dacf90126fde8fc39
piyusha-chopra <piyushachopra1909@gmail.com>
2025-08-24 18:23:20 +0530

    Update script.js

--- Spotify Clone/script.js
+++ Spotify Clone/script.js
@@ -5,7 +5,7 @@ let currentFolder;
 
 async function getSongs(folder) {
     currentFolder=folder
-    let a =await fetch (`http://127.0.0.1:3000/${folder}/`)
+    let a =await fetch (`/${folder}/`)
     let response=await a.text()
     
     let div=document.createElement("div")
@@ -59,7 +59,7 @@ const playMusic=(track,pause=false)=>{
 }
 
 async function displayAlbums(){
-    let a =await fetch(`http://127.0.0.1:3000/songs/`)
+    let a =await fetch(`/songs/`)
     let response=await a.text()
  
     let div=document.createElement("div")
@@ -72,7 +72,7 @@ async function displayAlbums(){
         if(e.href.includes("/songs")){
             let folder=e.href.split("/").slice(-2)[0]
 
-            let a =await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`)
+            let a =await fetch(`/songs/${folder}/info.json`)
             let response=await a.json()
         
             cardContainer.innerHTML=cardContainer.innerHTML+`<div data-folder="${folder}" class="card">
